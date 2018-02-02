85d9dc2d1b2543adb27a8cac5c5b9f7524baf7df
Dmitriy <jarenkovd@mail.ru>
2018-02-02 17:57:24 +0300

    3com

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
@@ -8,8 +9,8 @@ public class App {
     private EventLogger eventLogger;
 
     public static void main(String[] args) {
-        @SuppressWarnings("resource")           // We will remove this suppress in further lessons
-                ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");   //ConfigurableApplicationContext наследуется от ApplicationContext
         App app = (App) ctx.getBean("app");
 
         Event event = ctx.getBean(Event.class); //нахождение по классу
@@ -17,6 +18,8 @@ public class App {
 
         event = ctx.getBean(Event.class);
         app.logEvent(event, "Some event for 2");
+
+        ctx.close();
     }
 
     public App(Client client, EventLogger eventLogger) {
---
+++ src/test/java/TestEvent.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertTrue;
+import java.text.DateFormat;
+import java.util.Date;
+import org.junit.Test;
+
+public class TestEvent {
+
+    @Test
+    public void testToString() {
+        Date date = new Date();
+        DateFormat format = DateFormat.getDateInstance();
+        Event event = new Event(date, format);
+        String str = event.toString();
+        assertTrue(str.contains(format.format(date)));
+    }
+
+    @Test
+    public void testToString2() {
+        Date date = new Date();
+        DateFormat format = DateFormat.getDateTimeInstance();
+        Event event = new Event(date, format);
+        String str = event.toString();
+        assertTrue(str.contains(format.format(date)));
+    }
+
+}
